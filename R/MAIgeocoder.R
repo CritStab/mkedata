c2d64a55b9ecdea11a0392bccd6fbceb26fda4c7
Matthew Schumwinger <milwaukeestat@gmail.com>
2016-04-15 13:07:10 -0500

    add get_parcels function

--- R/MAIgeocoder.R
+++ R/MAIgeocoder.R
@@ -67,7 +67,7 @@ get_mai <- function(url = mai_url){
 #' @export
 #' @examples
 #' \dontrun{
-#' raw_geo <- geocode(head(raw), "LOCATION")
+#' raw_geo <- geocode_api(head(raw_wibrs_2005thru2015), "LOCATION")
 #' raw_geo
 #' }
 #'
@@ -128,3 +128,41 @@ geocode_api <- function(batch, fields){
           , "%)", sep="")
   batch
 }
+
+
+
+# match_mai <- (){
+#   sts <- as.data.frame(table(crime$LOCATION)) # table of unique address strings, for validation
+#
+#   #####################
+#   ## PREPROCESS DATA ##
+#   #####################
+#   # note: this is high volume and may be rejected by City API
+#   # send to geocoder API only those sts not matched by MAI
+#   # note any MAI score less than 95 could be suspect, especially on street direction error, DIME over 90 look good
+#   # records with score 100 about 21% ; records with score >= 95 about 29%
+#
+#   # merge MAI geo data to crime records where possible
+#   crime <- as.data.table(crime)
+#   mai <- as.data.table(mai)
+#   setkey(crime, LOCATION)
+#   setkey(mai, ADDRESS)
+#   mai.u <- unique(mai)
+#   crimeMAI <- mai.u[crime] # left outer join
+#   table(is.na(crimeMAI$WKT)) # compare to:
+#   table(crime$LOCATION %in% mai$ADDRESS) # direct match rate to MAI: ~ 78%
+#   crimeMAI <- crimeMAI[, c(1:3,18:28), with=F] # remove unessessry columns
+#
+#   # unique unmatched addresses
+#   setkey(crimeMAI, ADDRESS)
+#   u <- unique(crimeMAI) # unique addresses (should match sts)
+#   uu <- u[is.na(u$WKT), ] # and unmatched
+#
+#   # cull bad stuff known to fail geocoder
+#   uu <- uu[!grep("/", uu$ADDRESS), ] # intersections generally fail in City API
+#
+#   # split by district to feed chunks to geocoder
+#   d <- uu$DISTRICT
+#   uu.split <- split(as.data.frame(uu), d, drop=TRUE)
+#   #test <- lapply(uu.split, head, 2)
+# }
---
+++ R/spatial.R
@@ -0,0 +1,59 @@
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#
+# This document contains functions that retreive spatial data for Milwaukee,
+# Wisconsin.
+#
+# The primary functions include:
+#  o get_parcelbase
+#  o get_mprop
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+#' get_parcels
+#'
+#' get_parcelbase is a function that retreives the City of Milwaukee's parcel shapefile
+#' \href{http://city.milwaukee.gov/DownloadMapData3497.htm}{from the
+#' city's download map data page}.
+#'
+#' The City of Milwaukee parcel shapefile accommodates condominium records
+#' (multiple tax identifiers -- "taxkeys" -- assigned to one parcel).  Multiple
+#' records (one for each taxkey) exist for condominium parcels. As a result, a
+#' direct join can be made between parcelbase.shp and MPROP (or other
+#' taxkey-based dataset), and condominium data will be accurately joined.
+#'
+#' @importFrom rgdal readOGR
+#'
+#' @param url Endpoint URL. Default = \url{http://itmdapps.milwaukee.gov/gis/mapdata/parcelbase.zip}
+#' @return A large SpatialPolygonsDataFrame.
+#' @export
+#' @examples
+#' \dontrun{
+#' parcels <- get_parcels()
+#' head(parcels@data)
+#' }
+#'
+get_parcels <- function(url = parcel_url){
+  message("This retrieves the parcelbase shapefile, which is approximately 16MB and may
+          take awhile to download.")
+  message("Downloading . . . ")
+
+  retrieved <- Sys.Date()
+  parcel_url <- "http://itmdapps.milwaukee.gov/gis/mapdata/parcelbase.zip" # as of Apr 15 2016
+  mainDir <- getwd()
+  subDir <- "temp_downloads"
+  path <- paste(mainDir, subDir, sep = "/")
+
+  temp <- tempfile()
+  download.file(parcel_url, temp)
+  dir.create(file.path(mainDir, subDir), showWarnings = FALSE)
+  unzip(temp, exdir = path)
+
+  message("Download complete . . . ")
+  message("Transforming shapefile to SpatialPolygonsDataFrame. . . .")
+  spdf <- rgdal::readOGR(path, "parcelbase")
+  unlink(temp)
+  unlink(path, recursive = T)
+  message("Removing `/temp_downloads` directory . . .")
+  spdf
+}
