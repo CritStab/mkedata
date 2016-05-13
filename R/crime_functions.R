d4a5cc360bd0b68519d1cff82f91b543c4d5e6af
Matthew Schumwinger <milwaukeestat@gmail.com>
2016-05-13 15:01:38 -0500

    add and document crimes_by_group_year fucntion

--- R/crime_functions.R
+++ R/crime_functions.R
@@ -194,13 +194,9 @@ munge_wibrs <- function(df) {
   df$year <- format(df$CDATE, "%Y")
 
   # transform to sp class and set CRS
-  NAD27 <-
-    sp::CRS(
-      "+proj=lcc +lat_1=42.73333333333333 +lat_2=44.06666666666667
-      +lat_0=42 +lon_0=-90 +x_0=609601.2192024384 +y_0=0 +datum=NAD27
-      +units=us-ft +no_defs +ellps=clrk66 +nadgrids=@conus,@alaska,
-      @ntv2_0.gsb,@ntv1_can.dat"
-    )
+  NAD27 <- sp::CRS("+proj=lcc +lat_1=42.73333333333333 +lat_2=44.06666666666667 +lat_0=42 +lon_0=-90
+             +x_0=609601.2192024384 +y_0=0 +datum=NAD27 +units=us-ft +no_defs +ellps=clrk66
+             +nadgrids=@conus,@alaska,@ntv2_0.gsb,@ntv1_can.dat")
 
   crimes <- df
   crimes <- subset(crimes, !is.na(y))
@@ -224,4 +220,61 @@ munge_wibrs <- function(df) {
   )
 
   return(crimes)
+}
+
+
+#' crimes_by_group_year
+#'
+#' crimes_by_group_year aggregates munged geo-processed WIBRS data by offense
+#' group and by year.
+#'
+#' @importFrom data.table data.table melt dcast
+#' @importFrom sp CRS coordinates proj4string plot SpatialPointsDataFrame
+#' @param spdf A SpatialPointsDataFrame of WIBRS data.
+#' @return A SpatialPointsDataFrame in CRS NAD27.
+#' @export
+#' @examples
+#' data(crimes.munged)
+#' agg <- crimes_by_group_year(crimes.munged)
+
+crimes_by_group_year <- function(spdf){
+
+  # ## group by address, and sum by group  (all years)
+  # out <- data.table::data.table(crimes.munged@data)
+  # cols <- c("ADDRESS","x","y","group")
+  #
+  # s.all <- subset(out, select=cols)
+  # l.all <- data.table::melt(s.all, id.vars=c("ADDRESS","x","y"))
+  # w.all <- data.table::dcast(l.all, ADDRESS + x + y ~ value,
+  #                            fun.aggregate = length, margins=F)
+  # w.all <- data.frame(w.all)
+  # w.all$total <- apply(w.all[4:13], 1, sum)
+
+  ## group by address and year, and sum by group
+  spdf <- crimes.munged
+  projection <- spdf@proj4string
+  out <- data.table::data.table(spdf@data)
+  cols <- c("ADDRESS","x","y","group", "year")
+  s.all <- subset(out, select=cols)
+  l.all <- data.table::melt(s.all, id.vars=c("ADDRESS","x","y","year"))
+  w.all <- data.table::dcast(l.all, ADDRESS + x + y + year ~ value,
+                             fun.aggregate = length, margins=F)
+  w.all <- data.frame(w.all)
+  w.all$total <- apply(w.all[5:14], 1, sum)
+
+
+  w.all$x <- as.numeric(w.all$x)
+  w.all$y <- as.numeric(w.all$y)
+  newspdf <- sp::SpatialPointsDataFrame(coords = w.all[, c("x", "y")], data = w.all, proj4string = projection)
+
+  # confirm with plot
+  sp::plot(
+    newspdf[sample(1:length(newspdf), 1000), ],
+    add = F,
+    col = "light blue",
+    main = paste0("SpatialPointsDataFrame in NAD27" , "\nN = ",
+                  dim(newspdf@data)[1])
+  )
+
+  return(newspdf)
 }
